b21f537b71cda63b15ae1021e93a2119a234b6e6
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2024-01-25 06:58:03 +0530

    Worked further

--- src/App.js
+++ src/App.js
@@ -7,30 +7,46 @@ function App() {
       <h3>Expense tracker </h3>
       <div className="balance-operations">
         <p>Balance Rs5000</p>
-        <button>Add</button>
+        <button className="addButton">Add</button>
       </div>
       <div className="adding-expenses">
-        <div className="input-fields">
-          <input type="number" placeholder=" Enter Amount" />
-          <input type="text" placeholder="Enter Details" />
-        </div>
-        <div className="expense-budget">
-          <input
-            type="radio"
-            id="html"
-            name="fav_language"
-            value="HTML"
-          ></input>
-            <label for="html">Expense</label>
-          <input
-            type="radio"
-            id="html"
-            name="fav_language"
-            value="HTML"
-          ></input>
-            <label for="html">Budget</label>
+        <input type="number" placeholder=" Enter Amount" />
+        <input type="text" placeholder="Enter Details" />
+        <div className="radioButtons-expensebudget">
+          <div className="radioButton-expense">
+            <input
+              type="radio"
+              id="html"
+              name="fav_language"
+              value="HTML"
+            ></input>
+              <label for="html">Expense</label>
+          </div>
+          <div className="radioButton-budget">
+            <input
+              type="radio"
+              id="html"
+              name="fav_language"
+              value="HTML"
+            ></input>
+              <label for="html">Budget</label>
+          </div>
         </div>
         <button>Add Transaction</button>
+      </div>
+      <div className="expensebudget-result">
+        <div className="expense-result">
+          <p>Expense</p>
+          <p>Rs500</p>
+        </div>
+        <div className="budget-result">
+          <p>Expense</p>
+          <p>Rs500</p>
+        </div>
+      </div>
+      <div className="transactions">
+        <p className="transaction">Transactions</p>
+        <p className="searchbar">Search here...</p>
       </div>
     </div>
   );
